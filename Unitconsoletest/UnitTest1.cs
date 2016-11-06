b3e5b0e43818dc7ea6994ae98fcc5de222b16908
venkatkrp <venkatkrp@gmail.com>
2016-11-06 16:55:45 +0530

    commit

---
+++ Unitconsoletest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unitconsoletest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("hello world", "hello world");
+        }
+    }
+}
